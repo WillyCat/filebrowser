f70477fe7bb0f0b1ee21b43ae170c4ffd9d951a1
WillyCat <grosminet750@gmail.com>
2020-07-24 17:22:52 +0200

    0.2
    + upload

--- index.php
+++ index.php
@@ -5,11 +5,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
     <meta name="author" content="">
-    <link rel="icon" href="/docs/4.0/assets/img/favicons/favicon.ico">
+    <link rel="icon" href="ico/darva.png">
 
-    <title>Dashboard Template for Bootstrap</title>
+    <title>File browsing</title>
 
-    <link rel="canonical" href="https://getbootstrap.com/docs/4.0/examples/dashboard/">
+<!--
+    Based on getbootstrap.com/docs/4.0/examples/dashboard/
+-->
 
     <!-- Bootstrap core CSS -->
 <link rel="stylesheet" href="lib/bootstrap/css/bootstrap.min.css">
@@ -32,6 +34,8 @@ $fw_conf['ldap']['pattern']= 'cn={login},ou=DAP,ou=DOP,dc=darva,dc=intra';
 
 $npp = 10;
 
+$columns = [ 'Filename', 'Size', 'Perms', 'mtime', 'ctime', 'type' ];
+
 //----------------------------------------------------------
 // from https://stackoverflow.com/questions/2510434/format-bytes-to-kilobytes-megabytes-gigabytes
 function
@@ -49,6 +53,8 @@ formatBytes(int $bytes, int $precision = 2):string
 
     return round($bytes, $precision) . ' ' . $units[$pow]; 
 } 
+
+//----------------------------------------------------------
 
 function
 getperms (string $pathname): string
@@ -102,11 +108,12 @@ getperms (string $pathname): string
 		    (($perms & 0x0200) ? 'T' : '-'));
 
 	return $info;
-
 }
 
+//----------------------------------------------------------
+
 function
-show_login_form(): string
+show_login_form(): void
 {
 echo '
 <body>
@@ -117,11 +124,10 @@ echo '
 	<form method="post" action="index.php">
 		<fieldset>
 		<legend>Login</legend>
-		<p><label>Login<br><input type="text" name="login" size="30" /></label></p><br>
-		<p><label>Password<br><input type="password" name="pass" size="30" /></label></p><br>
+		<p><label>Login<br><input type="text" name="login" size="30" /></label></p>
+		<p><label>Password<br><input type="password" name="password" size="30" /></label></p>
 		<p><div id="authzone"><input id="authbutton" type="submit" value="Authenticate" /></div></p></center>
 		</fieldset>
-<input type="hidden" name="dest" value="<?=$dest ?>">
 	</form>
 </TD>
 </TR>
@@ -132,20 +138,173 @@ echo '
 ';
 }
 
+function
+display_column (array $file, string $column): void
+{
+	global $path;
+
+	$a_open = false;
+
+	echo '<td>';
+	switch ($column)
+	{
+	case 'Filename' :
+		if ($file['type'] == 'dir')
+		{
+			echo '<a href="?path='.$path.'/' . $file['name'].'">';
+			$a_open = true;
+		}
+		if ($file['type'] == 'link')
+			if (is_dir ($file['target']))
+			{
+				echo '<a href="?path='.$file['target'].'" title="'.$file['target'].'">';
+				$a_open = true;
+			}
+		echo $file['name'];
+		if ($file['type'] == 'dir')
+			echo '/';
+		break;
+	case 'Size' :
+		if ($file['type'] == 'file')
+		{
+			echo '<span title="'.$file['size'].'">';
+			echo $file['formattedsize'];
+			echo '</span>';
+		}
+		break;
+	case 'Perms' :
+		echo $file['perms'];
+		break;
+	case 'mtime' :
+		echo $file['mtime-display'];
+		break;
+	case 'ctime' :
+		echo $file['ctime-display'];
+		break;
+	case 'type' :
+		echo $file['type'];
+		break;
+	default :
+		break;
+	}
+	if ($a_open)
+		echo '</a>';
+	echo '</td>';
+}
+
+function
+display_line (array $file, array $columns): void
+{
+	echo '<tr>';
+	foreach ($columns as $column)
+		display_column ($file, $column);
+	echo '</tr>';
+}
+
+function
+make_link (int $page, string $linkpath = ''): string
+{
+	if ($linkpath == '')
+	{
+		global $path;
+		$linkpath = $path;
+	}
+
+	$link = '?path='.$linkpath.'&page='.$page;
+
+	return $link;
+}
+
+function
+show_page (int $page, int $pageno): void
+{
+	global $path;
+
+	if ($page == ($pageno+1) )
+		$item_attributes = 'active';
+	else
+		$item_attributes = '';
+
+	$page_link = make_link ($page);
+?>    
+    <li class="page-item <?=$item_attributes ?>"><a class="page-link" href="<?=$page_link?>"><?=$page ?></a></li>
+<?php
+}
+
+function
+show_pagination (int $pageno, int $nbpages): void
+{
+	global $path;
+
+	if ($pageno == 0)
+		$prev_attributes = 'disabled';
+	else
+		$prev_attributes = '';
+	$prev_link = make_link ($pageno);
+
+	if (($pageno+1) >= $nbpages)
+		$next_attributes = 'disabled';
+	else
+		$next_attributes = '';
+	$next_link = make_link ($pageno+2);
+
+?>
+<nav aria-label="Page navigation">
+  <ul class="pagination justify-content-end">
+    <li class="page-item <?=$prev_attributes ?>">
+      <a class="page-link" href="<?=$prev_link?>" tabindex="-1">Previous</a>
+    </li>
+<?php
+	for ($page = 1; $page <= $nbpages; $page++)
+		show_page ($page, $pageno);
+ ?>
+    <li class="page-item <?=$next_attributes ?>">
+      <a class="page-link" href="<?=$next_link?>">Next</a>
+    </li>
+  </ul>
+</nav>
+<?php
+}
+
+//----------------------------------------------------------
+
+$upload_status = '';
+
 session_start();
+
+if (array_key_exists ('action', $_POST) && $_POST['action'] == 'upload')
+{
+	// TODO: select path from session instead
+	$path = $_POST['path'];
+
+	$name = $_FILES["fileToUpload"]["name"];
+	$size = $_FILES["fileToUpload"]["size"];
+	$ext  = strtolower(pathinfo($name,PATHINFO_EXTENSION));
+
+	$target_file = $path . '/' . $name;
+
+	if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file))
+	{
+		$upload_status = 'OK';
+	}
+	else
+	{
+		$upload_status = 'NOK';
+	}
+}
 
 if (array_key_exists ('login', $_POST))
 {
-$login = $_POST['login'];
-$password = $_POST['password'];
+	$login = $_POST['login'];
+	$password = $_POST['password'];
 
-if (!function_exists ('ldap_connect'))
-{
-	echo 'missing ldap_connect';
-	die (1);
-}
+	if (!function_exists ('ldap_connect'))
+	{
+		echo 'missing ldap_connect';
+		die (1);
+	}
 
-$ldapconn = ldap_connect($fw_conf['ldap']['server'],$fw_conf['ldap']['port']);
+	$ldapconn = ldap_connect($fw_conf['ldap']['server'],$fw_conf['ldap']['port']);
 	if (!$ldapconn)
 	  die( 'Cannot connect to [' . $fw_conf['ldap']['server'] . ']');
 
@@ -153,8 +312,13 @@ $ldapconn = ldap_connect($fw_conf['ldap']['server'],$fw_conf['ldap']['port']);
 	ldap_set_option($ldapconn, LDAP_OPT_REFERRALS,0);
 
 	$pattern = $fw_conf['ldap']['pattern'];
-	str_replace ('{login}', $login);
-	$lb = ldap_bind($ldapconn,$dn,$pass);
+	$dn = str_replace ('{login}', $login, $pattern);
+
+
+	//echo "ldap_bind($ldapconn,$dn,$password);";
+	$lb = true;
+
+	// $lb = ldap_bind($ldapconn,$dn,$password);
 	ldap_close($ldapconn);
 
 	if ($lb === true)	// LDAP - OK
@@ -204,6 +368,7 @@ if (!$dh)
 
 clearstatcache();
 $files = [];
+$size_used = 0;
 $i = 0;
 while (($file = readdir ($dh)) !== false)
 {
@@ -240,6 +405,8 @@ while (($file = readdir ($dh)) !== false)
 			$a['target'] = realpath($path . '/' . $linktarget);
 	}
 	$files[$i++] = $a;
+	if ($a['type'] == 'file')
+		$size_used += $a['size'];
 }
 
 $n = count ($files);
@@ -271,20 +438,51 @@ usort ($files, function ($a, $b) { return strcasecmp($a['name'], $b['name']); })
         <nav class="col-md-2 d-none d-md-block bg-light sidebar">
           <div class="sidebar-sticky">
             <ul class="nav flex-column">
+
               <li class="nav-item">
                 <a class="nav-link active" href="#">
                   <span data-feather="file"></span>
-                  Files
+                  Entries: <?=count($files)?>
                 </a>
               </li>
+
+		<!-- https://feathericons.com/ -->
+              <li class="nav-item">
+                <a class="nav-link active" href="#">
+                  <span data-feather="database"></span>
+                  Space: <?=formatBytes($size_used,2)?>
+                </a>
+              </li>
+
               <li class="nav-item ">
                 <a class="nav-link" href="?action=logout">
                   <span data-feather="log-out"></span>
                   Logout <?=$_SESSION['filebrowseruser'] ?>
                 </a>
               </li>
+
             </ul>
+<?php
+switch ($upload_status)
+{
+case '' : // no upload
+	break;
+case 'OK':  // successful upload
+	echo ' <div class="alert alert-success" role="alert"> UPLOAD SUCCESS </div>';
+	break;
+case 'NOK' : // failed upload
+	echo ' <div class="alert alert-danger" role="alert"> UPLOAD FAILURE </div>';
+	break;
+}
+?>
+<form action="index.php" method="post" enctype="multipart/form-data">
+<input type="hidden" name="action" value="upload">
+<input type="hidden" name="path" value="<?=$path ?>">
+  <input type="file" name="fileToUpload" id="fileToUpload">
+  <input type="submit" value="Upload Image" name="submit">
+</form>
           </div>
+
         </nav>
 
         <main role="main" class="col-md-9 ml-sm-auto col-lg-10 pt-3 px-4">
@@ -306,97 +504,32 @@ usort ($files, function ($a, $b) { return strcasecmp($a['name'], $b['name']); })
 		}
 		else
 		*/
-		echo '<li class="breadcrumb-item"><A HREF="?path='.$linkpath.'">' . $dirlevel . '</a></li>';
+		$link = make_link (1,$linkpath);
+		echo '<li class="breadcrumb-item"><A HREF="' . $link . '">' . $dirlevel . '</a></li>';
 	}
 	?>
 	</ul>
 	</div>
 	<div>
-		<span><?=count($files)?> file(s)</span>
 <?php
-if ($pageno == 0)
-	$prev_attributes = 'disabled';
-else
-	$prev_attributes = '';
-if (($pageno+1) >= $nbpages)
-	$next_attributes = 'disabled';
-else
-	$next_attributes = '';
 ?>
-<nav aria-label="Page navigation">
-  <ul class="pagination justify-content-end">
-    <li class="page-item <?=$prev_attributes ?>">
-      <a class="page-link" href="?path=<?=$path?>&page=<?=$pageno?>" tabindex="-1">Previous</a>
-    </li>
-<?php
-	for ($page = 1; $page <= $nbpages; $page++) {
-	if ($page == ($pageno+1) )
-		$item_attributes = 'active';
-	else
-		$item_attributes = '';
-	$page_link = '?path='.$path.'&page='.$page;
-?>    
-    <li class="page-item <?=$item_attributes ?>"><a class="page-link" href="<?=$page_link?>"><?=$page ?></a></li>
-<?php } ?>
-    <li class="page-item <?=$next_attributes ?>">
-      <a class="page-link" href=""?path=<?=$path?>&page=<?=$pageno+2?>">Next</a>
-    </li>
-  </ul>
-</nav>
+<?php show_pagination ($pageno, $nbpages); ?>
 	</div>
 
           <div class="table-responsive">
             <table class="table table-striped table-sm">
               <thead>
                 <tr>
-                  <th>Filename</th>
-                  <th>Size</th>
-                  <th>Perms</th>
-                  <th>mtime</th>
-                  <th>ctime</th>
-                  <th>type</th>
+<?php
+foreach ($columns as $column)
+	echo '<th>' . $column . '</th>';
+?>
                 </tr>
               </thead>
               <tbody>
 <?php
-//foreach ($files as $file)
 for ($i = ($pageno * $npp); $i < min($n, ($pageno+1)*$npp-1); $i++)
-{
-	$file = $files[$i];
-	echo '<tr>';
-	echo '<td>';
-	$a_open = false;
-	if ($file['type'] == 'dir')
-	{
-		echo '<a href="?path='.$path.'/' . $file['name'].'">';
-		$a_open = true;
-	}
-	if ($file['type'] == 'link')
-		if (is_dir ($file['target']))
-		{
-			echo '<a href="?path='.$file['target'].'" title="'.$file['target'].'">';
-			$a_open = true;
-		}
-	echo $file['name'];
-	if ($file['type'] == 'dir')
-		echo '/';
-	if ($a_open)
-		echo '</a>';
-	echo '</td>';
-	echo '<td>';
-	if ($file['type'] == 'file')
-	{
-		echo '<span title="'.$file['size'].'">';
-		echo $file['formattedsize'];
-		echo '</span>';
-	}
-	echo '</td>';
-	echo '<td>' . $file['perms'] . '</td>';
-	echo '<td>' . $file['mtime-display'] . '</td>';
-	echo '<td>' . $file['ctime-display'] . '</td>';
-	echo '<td>' . $file['type'] . '</td>';
-	echo '</tr>';
-}
+	display_line ($files[$i], $columns);
 ?>
               </tbody>
             </table>
