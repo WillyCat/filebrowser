e377ad528fe1ee6cf1d90d3da614b7020616518d
WillyCat <francois.houisse@gmail.com>
2020-08-06 12:39:05 +0200

    pull-image does not require docker login

--- pull-image.sh
+++ pull-image.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
+IMAGE=willycat/filebrowser
+docker pull $IMAGE
 # derived from https://stackoverflow.com/questions/26423515/how-to-automatically-update-your-docker-containers-if-base-images-are-updated
 set -e
-IMAGE=willycat/filebrowser
 NAME=fb
 CID=$(docker ps | grep ${NAME} | awk '{print $1}')
-docker login
-docker pull $IMAGE
+#docker login
 
 for im in $CID
 do
